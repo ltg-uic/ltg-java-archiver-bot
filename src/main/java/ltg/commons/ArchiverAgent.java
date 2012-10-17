177c3b053fc3238ed7335a778bef73c3118f1414
Alessandro Gnoli <tebemis@gmail.com>
2012-10-17 11:47:03 -0500

    Starting coding the agent

--- src/main/java/ltg/commons/ArchiverAgent.java
+++ src/main/java/ltg/commons/ArchiverAgent.java
@@ -8,16 +8,44 @@ package ltg.commons;
  *
  */
 public class ArchiverAgent {
+	
+	private String agentUsername = null;
+	private String agentPassword = null;
+	private String chatRoom = null;
+	private String dbName = null;
+	
+	// Create an XMMP client based on CLI parameters
+	SimpleXMPPClient xmpp = null;
 
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// Parse command line arguments
-		
-		// Create an XMMP client based on CLI parameters
-		SimpleXMPPClient xmpp = new SimpleXMPPClient("archiver", "archiver", "fg-game");
+		ArchiverAgent aa = new ArchiverAgent();
+		if (!aa.parseCLIArgs(args)) {
+			System.out.println("Print use");
+		}
+		aa.initializeAgent();
+		aa.startAgent();
 	}
+	
+	
+	private boolean parseCLIArgs(String[] args) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	private void initializeAgent() {
+		xmpp = new SimpleXMPPClient("archiver", "archiver", "fg-game");
+	}
+	
+	
+	private void startAgent() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
