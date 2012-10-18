d56643d72759ff70ec1490503ea51228e81b2734
Alessandro Gnoli <tebemis@gmail.com>
2012-10-18 11:39:10 -0500

    Main methods for agents are up and need the fine coding

--- src/main/java/ltg/commons/ArchiverAgent.java
+++ src/main/java/ltg/commons/ArchiverAgent.java
@@ -3,6 +3,16 @@
  */
 package ltg.commons;
 
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+
 /**
  * @author tebemis
  *
@@ -13,9 +23,15 @@ public class ArchiverAgent {
 	private String agentPassword = null;
 	private String chatRoom = null;
 	private String dbName = null;
+	private String dbHost = null;
+	private static String DEFAULT_MONGO_HOST = "localhost";
 	
-	// Create an XMMP client based on CLI parameters
-	SimpleXMPPClient xmpp = null;
+	// Logger
+	private Logger log = LoggerFactory.getLogger(getClass()); 
+	// XMMP client
+	private SimpleXMPPClient xmpp = null;
+	// Mongo database collection
+	private DBCollection mongo = null;
 
 
 	/**
@@ -32,19 +48,42 @@ public class ArchiverAgent {
 	
 	
 	private boolean parseCLIArgs(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// If on dbHost!!!
 		return true;
 	}
 
 
 	private void initializeAgent() {
-		xmpp = new SimpleXMPPClient("archiver", "archiver", "fg-game");
+		xmpp = new SimpleXMPPClient(agentUsername, agentPassword, chatRoom);
+		Mongo m = null;
+		DB db = null;
+		try {
+			m = new Mongo(dbHost);
+			db = m.getDB(dbName);
+			mongo = db.getCollection("log");
+		} catch (UnknownHostException e) {
+			log.error("Impossible to connect to MongoDB, terminating");
+			Thread.currentThread().interrupt();
+		}
 	}
 	
 	
 	private void startAgent() {
-		// TODO Auto-generated method stub
-		
+		while (!Thread.currentThread().isInterrupted()) {
+			storeEvent(xmpp.nextEvent());
+		}
+		xmpp.disconnect();
+	}
+
+
+	private void storeEvent(Event e) {
+		BasicDBObject doc = new BasicDBObject();
+		doc.put("origin", e.origin);
+		// If it's json insert the payload as json otherwise, just record the payload
+		// Pay attention to special characters
+		// maybe log too???
+		mongo.insert(doc);
 	}
 
 
